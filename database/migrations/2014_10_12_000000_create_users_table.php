d55d69858a8eec7296c2ef9523b36415a14b4234
Romário Arruda <romarioarruda98@gmail.com>
2021-05-04 20:10:10 -0300

    atualizando migration usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,7 @@ class CreateUsersTable extends Migration
                 $table->id();
                 $table->string('name');
                 $table->string('email')->unique();
-                $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
-                $table->text('token');
-                $table->timestamps();
             });
         }
     }
